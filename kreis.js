d198571efa880c8559966216e37a7bfc52848a3d
SimHox <simon.hoy@gmx.net>
2018-05-24 19:58:53 +0200

    / get via url

--- kreis.js
+++ kreis.js
@@ -1,25 +1,31 @@
 var fs = require('fs');//file system//zugriff aufs dateisystem
 var express = require('express');
 var bp = require('body-parser');//INhalte aus Request auslesen!!
+var fs = require('fs');
 
 var app = express();
 var server = app.listen(3008,function(){
 console.log('server mit express auf port 3008 gestartet');
 });
+/*
+app.use(function(request,response,next){//use =egal welcher request kommt
+    response.setHeader('Access-Control-Allow-Origin','*');//von jeder source//alternativ auf ip adressen einschränken
+    response.setHeader('Access-Control-Allow-Methods','OPTIONS,POST');//welche methode
+    response.setHeader('Access-Control-Request-Headers','Content-Type');
+    next();//bleibt in use hängen sonst
 
-app.use(function(request,response,next){
-    response.setHeader('Access-Control-Allow-Origin','*');//von jeder source
-    response.setHeader('Access-Control-Allow-Methods','POST');//welche methode
-
-});//use =egal welcher request kommt
-
+});
+*/
 
 app.post('/circle',function(request,response){
-console.log('POST Request an server');
-console.log(request.body);
-response.end( 'Test');
+    console.log('POST Request an server');
+    console.log(request.body);
+    response.end( 'Test');
 
 });
 app.get('/',function(request,response){
-    response.end('nichts vorhanden');
-})
+    response.sendFile(__dirname+'/1KreisAPI.html');//__dirname = aktueller ordnername//.. == ein ordner höher?
+});
+app.get('/j',function(request,response){
+    response.sendFile(__dirname+'/jquery-3.3.1.min.js');//__dirname = aktueller ordnername//.. == ein ordner höher?
+});
