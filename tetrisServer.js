be5766b8a8157e8caf83b27fe9b1336d95530155
SimHox <simon.hoy@gmx.net>
2018-05-27 11:32:26 +0200

    added / to url path

--- tetrisServer.js
+++ tetrisServer.js
@@ -8,13 +8,14 @@ app.listen('3000',function(){
     console.log('server started on 3000');
 });
 
-
-app.get('/j',function(request,response){
-    response.sendFile(__dirname+'jquery-3.3.1.min.js');
-
-})
 app.get('/',function(request,response){
     response.sendFile(__dirname+'/tetris/index.html');//__dirname = aktueller ordnername//.. == ein ordner höher?
     response.sendFile(__dirname+'/tetris/texture.jpg');
     response.sendFile(__dirname+'/tetris/stats.js');
+
 });
+
+app.get('/j',function(request,response){
+    response.sendFile(__dirname+'/jquery-3.3.1.min.js');
+
+})
