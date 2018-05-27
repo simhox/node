133e73903e020363af7c21af167d7f516aea66df
SimHox <simon.hoy@gmx.net>
2018-05-27 12:04:39 +0200

    added express-static for multiple file handling

--- tetrisServer.js
+++ tetrisServer.js
@@ -1,6 +1,7 @@
 bp = require("body-parser");
 express = require("express");
 fs = require('fs');
+serve = require("express-static");
 app = express();
 
 app.listen('3000',function(){
@@ -8,12 +9,16 @@ app.listen('3000',function(){
     console.log('server started on 3000');
 });
 
-app.get('/',function(request,response){
-    response.sendFile(__dirname+'/tetris/index.html');//__dirname = aktueller ordnername//.. == ein ordner höher?
-    response.sendFile(__dirname+'/tetris/texture.jpg');
-    response.sendFile(__dirname+'/tetris/stats.js');
+app.use(serve(__dirname+'/tetris'));
 
-});
+
+
+// app.get('/',function(request,response){
+//     // response.sendFile(__dirname+'/tetris/index.html');//__dirname = aktueller ordnername//.. == ein ordner höher?
+//     // response.sendFile(__dirname+'/tetris/texture.jpg');
+//     // response.sendFile(__dirname+'/stats.js');
+//     // console.log(__dirname+'/tetris/stats.js');
+// });
 
 app.get('/j',function(request,response){
     response.sendFile(__dirname+'/jquery-3.3.1.min.js');
