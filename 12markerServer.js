cc6d45ff5448a3c5153a6c44d83980813f263dfe
SimHox <simon.hoy@gmx.net>
2018-06-09 19:53:44 +0200

    updated id

--- 12markerServer.js
+++ 12markerServer.js
@@ -23,17 +23,38 @@ fs.readFile( '13marker.json', function( err, data ) {
   alleOrte = JSON.parse( data );
 } );
 var id = 0;
+
+var saveMarkersToFile = function(){
+
+    fs.writeFile( '13marker.json', JSON.stringify( alleOrte ), function() {
+        //könnte man noch was machen...
+    });
+
+}
+
+
 app.post('/addMarker',function(request,response){
     console.log('POST Request an server');
    //response.setHeader('Content-Type','application/json');
    var marker = request.body;
-   request.body.id = id;
-   id++;
+
+   // id++;
+
+   var max=0;
+   for(let i =0; i<alleOrte.eintraege.length; i++){
+       if(alleOrte.eintraege[i].id > max){
+           max = alleOrte.eintraege[i].id;
+       }
+   }
+   marker.id = ++max;
+   console.log(alleOrte.eintraege);
+   console.log(max);
    console.log(marker);
+
    alleOrte.eintraege.push(marker);
-   fs.writeFile( '13marker.json', JSON.stringify( alleOrte ), function() {
-       //könnte man noch was machen...
-    });
+   saveMarkersToFile();
+
+
 
    //var thejson = JSON.stringify(responseData);
     response.end(JSON.stringify(alleOrte.eintraege));
