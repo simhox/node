2ad9a9bc43a3796124a5c694f7033f575fb56f9f
SimHox <simon.hoy@gmx.net>
2018-07-14 22:43:26 +0200

    ajax  requests

---
+++ 20AquariumNode.js
@@ -0,0 +1,53 @@
+var fs = require('fs');//file system//zugriff aufs dateisystem
+var express = require('express');
+var bp = require('body-parser');//INhalte aus Request auslesen!!
+
+
+var app = express();
+var server = app.listen(3000,function(){
+console.log('server mit express auf port 3000 gestartet');
+});
+
+app.use(function(request,response,next){//use =egal welcher request kommt
+    response.setHeader('Access-Control-Allow-Origin','*');//von jeder source//alternativ auf ip adressen einschränken
+    response.setHeader('Access-Control-Allow-Methods','OPTIONS,POST,GET,DELETE');//welche methode
+    response.setHeader('Access-Control-Request-Headers','Content-Type');
+    response.setHeader('Access-Control-Allow-Headers','*');
+    next();//bleibt in use hängen sonst
+});
+var highscores =[];
+app.use(bp.json());//application-json
+app.post('/save',function(request,response){
+    console.log('POST Request an server (save)');
+    // console.log(request.body);
+    // console.log(request.body.id);
+   console.log(request.body);
+    highscores.push(request.body);
+    console.log(highscores);
+   //var thejson = JSON.stringify(responseData);
+    response.end(JSON.stringify(highscores));
+
+
+});
+
+app.post('/speciesSort',function(request,response){
+    console.log('POST Request an server (speciesSort)');
+    // console.log(request.body);
+    // console.log(request.body.id);
+    highscores.sort(function(a, b) {
+        return a["nmbOfSpecies"] - b["nmbOfSpecies"] || a["highscore"] - b["highscore"];
+    });
+   //var thejson = JSON.stringify(responseData);
+    response.end(JSON.stringify(highscores));
+});
+
+app.post('/highscoreSort',function(request,response){
+    console.log('POST Request an server (speciesSort)');
+    // console.log(request.body);
+    // console.log(request.body.id);
+    highscores.sort(function(a, b) {
+        return a["highscore"] - b["highscore"] || a["nmbOfSpecies"] - b["nmbOfSpecies"];
+    });
+   //var thejson = JSON.stringify(responseData);
+    response.end(JSON.stringify(highscores));
+});
